e725fada17b04e2ea09164a708c107f875cb0854
Harry <harry.j.wright@protonmail.com>
2024-10-31 16:10:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+# config.txt can be appended with echo
+echo "disable_splash=1" >> /boot/config.txt # disable rainbow splash screen
+echo "lcd_rotate=2" >> /boot/config.txt # rotate screen 180 deg
+
+# cmdline.txt needs to pass the no-newline argument
+echo -n "logo.nologo" >> /boot/cmdline.txt # remove pi logo on tty
+echo -n "vt.global_cursor_default=0" >> /boot/cmdline.txt # disable blinking cursor
+# echo -n "loglevel=3" >> /boot/cmdline.txt # mute kernel logs (show critical only)
+
+# must login on tty
+uci set system.@system[0].ttylogin="1"
+uci commit system
+service system restart
+
 # disable ipv6 https://3os.org/infrastructure/openwrt/disable-ipv6/
 uci set 'network.lan.ipv6=0'
 uci set 'network.wan.ipv6=0'
@@ -16,8 +30,3 @@ uci commit network
 uci -q delete network.globals.ula_prefix
 uci commit network
 /etc/init.d/network restart
-
-# must login on tty
-uci set system.@system[0].ttylogin="1"
-uci commit system
-service system restart
